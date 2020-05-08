ed669e1b5440049ed4c4c5cdcd70625e0845f704
matthew-heideman <heidemanmr@yahoo.com>
2020-05-08 11:28:00 -0500

    completed header

--- components/Header/index.js
+++ components/Header/index.js
@@ -11,4 +11,22 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+function Header() {
+    const container = document.createElement('div');
+    const title = document.createElement('h1');
+    const date = document.createElement('span');
+    const temp = document.createElement('span');
+    container.classList.add('header');
+    date.classList.add('date');
+    temp.classList.add('temp');
+    title.textContent = 'Lambda Times';
+    date.textContent = 'APRIL 10, 2020';
+    temp.textContent = '68°';
+    container.appendChild(date);
+    container.appendChild(title);
+    container.appendChild(temp);
+    return container;
+};
+
+const headerParent = document.querySelector('.header-container');
+headerParent.appendChild(Header());
